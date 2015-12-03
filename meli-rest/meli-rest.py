b5b9a5f87d3e295c1dfb58afd589e70b6ccf5986
Cristian <cristian.maluenda@mercadolibre.cl>
2015-12-03 18:22:19 -0300

    Soporte unicode

--- meli-rest/meli-rest.py
+++ meli-rest/meli-rest.py
@@ -6,9 +6,11 @@ import json
 import urllib2
 import getopt
 
-def test(k):
-	f = open('json.out', 'r')
-	return f.read()
+
+def read_file(name):
+    f = open(name, 'r')
+    return f.read()
+
 
 def call_api(url, content=None):
     try:
@@ -22,17 +24,17 @@ def call_api(url, content=None):
 
 
 def print_json(text):
-	try:
-		response = json.loads(text)
-		print json.dumps(response, sort_keys=False, indent=4, separators=(',', ': '))
-	except:
-		print "El texto no es un JSon válido:\n%s"%text
-		sys.exit()
+    try:
+        response = json.loads(text, 'utf-8')
+        print json.dumps(response, sort_keys=False, indent=4, separators=(',', ': '), encoding="utf-8", ensure_ascii=False)
+    except:
+        print "El texto no es un JSon válido:\n%s"%text
+        sys.exit()
 
 def parse_argv(argv):
     try:
         urls = []
-        opts, args = getopt.getopt(sys.argv[1:], 'ip',['url=','item-api=', 'content='])
+        opts, args = getopt.getopt(sys.argv[1:], 'ip',['url=','file=','item-api=', 'content='])
         opts = dict(opts)
         #print opts
         #print args
@@ -41,34 +43,43 @@ def parse_argv(argv):
     except Exception as e:
         print '''
         uso: %s [-p | -i] options
-        
+
         -p : Api pública
         -i : Api internal (default)
-        
+
         options:
         --url=URL : llamar a la API usando URL.
         --content=CONTENT : Llamado de la API con los parámetros CONTENT.
         --item-api=ITEM_ID : Detalle para el ITEM_ID
         '''%(sys.argv[0])
         sys.exit()
-        
+
     url = None
     content = None
-    
+    is_file = False
+
     if opts.get('--item-api'):
         itemId = opts['--item-api']
         url = "mercadolibre.com/items/%s"%itemId
-        
+
     if opts.get('--url'):
         url = opts['--url']
+    elif opts.get('--file'):
+        is_file = True
+        url = opts['--file']
     elif '-p' in opts:
         url = "https://api."+url
     else:
         url = "http://internal."+url
-    
-    return url, content
+
+    return url, content, is_file
 
 if __name__ == '__main__':
-    url, content = parse_argv(sys.argv[1:])
-    response = call_api(url, content)
+    uri, content, is_file = parse_argv(sys.argv[1:])
+
+    response = ''
+    if (is_file):
+        response = read_file(uri)
+    else:
+        response = call_api(uri, content)
     print_json(response)
